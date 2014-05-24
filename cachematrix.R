29cfad82d68ca957fb5a1a6c04cdff71ccb4d383
Cyphase <cyphase@cyphase.com>
2014-05-24 15:36:22 +0700

    Finished assignment
    Did the assignment. Instead of doing it exactly like in the example, I
    integrated the inverse calculation code into the special "matrix" object
    (so that everything lives in one place). Therefore, cacheSolve is just a
    thin wrapper at this point.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These functions implement caching of the inverse of a matrix, so that you
+# only have to compute the inverse of a given matrix once, even if you use it
+# multiple times.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# makeCacheMatrix takes a matrix as an argument and returns a list of functions
+# that act upon that matrix.
+makeCacheMatrix <- function(mat = matrix()) {
+  # We're creating a new object, so set the inverse to NULL
+  inv <- NULL
+  
+  set <- function(m) {
+    mat <<- m
+    inv <<- NULL
+  }
+  get <- function() mat
+  getinverse <- function(...) {
+    # If the inverse has not been calculated
+    if(is.null(inv)){
+      # Calculate and store the inverse
+      i <- solve(mat, ...)
+      inv <<- i
+    # Otherwise, if the inverse has been calculated
+    } else {
+      message("getting cached inverse matrix")
+    }
+    # Either way, at this point we can return the inverse
+    inv
+  }
+  
+  # Create and return the final list
+  list(set = set,
+       get = get,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve takes a special "matrix" object and returns the inverse.
+# Originally, this did more work, but that code has been integrated into the
+# special "matrix" object (so that everything lives in one place). Therefore,
+# cacheSolve is just a thin wrapper at this point.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  x$getinverse()
 }
